0fa014769c183c9cd99651a1dfcf9bbc7d84edb5
Jeremy Kemp <jakemp@uh.edu>
2013-07-01 17:08:12 -0500

    refactored into sw function

--- smith_waterman/sw_hpx.cpp
+++ smith_waterman/sw_hpx.cpp
@@ -21,6 +21,7 @@ unsigned long GetTickCount()
 
 int find_array_max(int array[],int length);
 char** openFile(char* fileName, unsigned int &N, unsigned int &num);
+void sw( int **H, char *seq_a, char *seq_b, int chunk_size, int size_a, int size_b);
 
 int ind, gap;
 
@@ -28,68 +29,72 @@ int main(int argc, char** argv)
 {
     if(argc!=5){
         printf("Please provide correct number of input arguments: \n");
-        printf("1:gap 2:filename sequence_A  3:filename sequence_B 4:chunk_size\n");
+        printf("1:gap 2:filename sequence_A  3:filename sequence_B 4:chunk_size_size\n");
         exit(0);
     }
     gap = atof(argv[1]);
-    int chunk = atoi(argv[4]);
-    unsigned int N_a, num_a, N_b, num_b;
-    char **seq_a = openFile(argv[2], N_a, num_a);
-    char **seq_b = openFile(argv[3], N_b, num_b);
+    int chunk_size = atoi(argv[4]);
+    unsigned int size_a, num_a, size_b, num_b;
+    char **seq_a = openFile(argv[2], size_a, num_a);
+    char **seq_b = openFile(argv[3], size_b, num_b);
     double t1,t2,total;
 
-    int **H = new int*[N_a + 1];
-    for(int i=0; i< N_a + 1; i++)
-        H[i] = new int[N_b + 1];
-    for(int i=0;i<=N_a;i++){
-        for(int j=0;j<=N_b;j++){
-            H[i][j]=0;
-        }
-    }
+    int **H = new int*[size_a + 1];
+    for(int i=0; i< size_a + 1; i++)
+        H[i] = new int[size_b + 1];
 
-    int temp[4];
-    int H_max = 0, i_max=0, j_max=0;
-    int current_i, current_j, next_i, next_j;
-    int tick=0;
-    int waves = N_a + N_b +1; 
-    int wave, elements, np, mp;
-    int min = 0;
-
-    for(int a=0; a < num_a; a++) {
-        for(int c=0;c<=N_a;c++){
-            for(int d=0;d<=N_b;d++){
+    for(int seq_num=0; seq_num < num_a; seq_num++) {
+        for(int c=0; c<=size_a; c++){
+            for(int d=0; d<=size_b; d++){
                 H[c][d]=0;
             }
         }
         t1 = omp_get_wtime();
-#pragma omp parallel firstprivate(a, gap, waves) private(temp, wave, i) shared(np, mp, elements)
+        sw(H, seq_a[seq_num], seq_b[seq_num], chunk_size, size_a, size_b);
+        t2 = omp_get_wtime();
+        total += t2 - t1; 
+    }
+    printf("Time for Smith-Watterman in secs: %f \n", total);
+}
+
+void sw( int **H, char *seq_a, char *seq_b, int chunk_size, int size_a, int size_b)
+{
+    int temp[4];
+    int H_max = 0, i_max=0, j_max=0;
+    int current_i, current_j, next_i, next_j;
+    int tick=0;
+    int num_waves = size_a + size_b +1; 
+    int wave, elements, np, mp;
+    int min = 0;
+
+#pragma omp parallel firstprivate(gap, num_waves) private(temp, wave) shared(np, mp, elements)
         {
 #pragma omp master
             {
-                for(wave = 0; wave < waves; ++wave) {
-                    if(wave < N_a-1) {
+                for(wave = 0; wave < num_waves; ++wave) {
+                    if(wave < size_a-1) {
                         elements = wave+1;
                         np = wave+1;
                         mp = 0+1;
-                    } else if(wave < N_b) {
-                        elements = N_a;
-                        np = N_a-1+1;
-                        mp = wave-(N_a-1)+1;
+                    } else if(wave < size_b) {
+                        elements = size_a;
+                        np = size_a-1+1;
+                        mp = wave-(size_a-1)+1;
                     } else {
-                        elements = N_a-1-(wave-N_b);
-                        np = N_a-1+1;
-                        mp = wave-(N_a-1)+1;
+                        elements = size_a-1-(wave-size_b);
+                        np = size_a-1+1;
+                        mp = wave-(size_a-1)+1;
                     }
-                    for(int ii = 0; ii < elements; ii+=chunk) {
-                        min = MIN(elements,ii + chunk);
-#pragma omp task firstprivate(ii, np, mp, chunk, elements)
+                    for(int ii = 0; ii < elements; ii+=chunk_size) {
+                        min = MIN(elements,ii + chunk_size);
+#pragma omp task firstprivate(ii, np, mp, chunk_size, elements)
                         {
                             for (int i = ii; i < min; i++) {
-                                temp[0] = H[(np-i)-1][(mp+i)-1] + similarity(seq_a[a][(np-i)-1],seq_b[a][(mp+i)-1]); 
+                                temp[0] = H[(np-i)-1][(mp+i)-1] + similarity( seq_a[(np-i)-1], seq_b[(mp+i)-1] );
                                 temp[1] = H[(np-i)-1][(mp+i)]-gap;                  
                                 temp[2] = H[(np-i)][(mp+i)-1]-gap;
                                 temp[3] = 0;
-                                H[(np-i)][(mp+i)] = find_array_max(temp,4);
+                                H[(np-i)][(mp+i)] = find_array_max( temp, 4 );
                             }
                         } 
                     } 
@@ -97,13 +102,8 @@ int main(int argc, char** argv)
                 }
             }
         }
-        t2 = omp_get_wtime();
-        total += t2 - t1; 
-    }
-    printf("Time for Smith-Watterman in secs: %f \n", total);
 }
-
-char** openFile(char* fileName, unsigned int &N, unsigned int &num)
+char** openFile(char* fileName, unsigned int &size, unsigned int &num)
 {
     char **seq;
     FILE *fp = fopen(fileName, "r");
@@ -111,11 +111,11 @@ char** openFile(char* fileName, unsigned int &N, unsigned int &num)
         printf("Error: could not open %s!\n", fileName);
         exit(0);
     }
-    fscanf(fp, "%d %d\n", &num, &N);
+    fscanf(fp, "%d %d\n", &num, &size);
     seq = new char*[num];
 
     for(int a=0; a < num; a++) {
-        seq[a] = new char[N+1];
+        seq[a] = new char[size+1];
         fscanf(fp, "%s\n", seq[a]);
     }
     fclose(fp);
