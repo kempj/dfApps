369a37bcf7bdb35e8cefb7ee1f7172b923308968
Jeremy Kemp <jakemp@uh.edu>
2013-07-14 18:33:29 -0500

    fixed error

--- lu/lu-hpx-dataflow.cpp
+++ lu/lu-hpx-dataflow.cpp
@@ -70,48 +70,36 @@ void LU( int size, int numBlocks)
     future<int> fsize = hpx::make_ready_future(size);
 
     for(int i = 0; i < numBlocks; i++){
-        dfArray[i].resize(numBlocks);
+        dfArray[i].resize( numBlocks );
         for(int j = 0; j < numBlocks; j++){
-            dfArray[i][j].resize(numBlocks, hpx::make_ready_future(block()));
+            dfArray[i][j].resize( numBlocks, hpx::make_ready_future( block()));
         }
     }
+    //converts blocks to futures for dataflow input in dfArray[0]
     dfArray[0][0][0] = async( ProcessDiagonalBlock, size, blockList[0][0] );
     diag_block = &dfArray[0][0][0];
     for(int i = 1; i < numBlocks; i++) {
-        dataflow( unwrapped( &ProcessBlockOnRow ), fsize, hpx::make_ready_future( blockList[0][i] ), *diag_block);
-        //dfArray[0][0][i] = when_all( make_ready_future( blockList[0][i] ), *diag_block).then( 
-        //                        unwrapped( [](vector<future<block>> const & fs){ return ProcessBlockOnRow( fs[0].get(), fs[1].get() ); } ));
+        dfArray[0][0][i] = dataflow( unwrapped( &ProcessBlockOnRow ), fsize, hpx::make_ready_future( blockList[0][i] ), *diag_block);
     }
     for(int i = 1; i < numBlocks; i++) {
         dfArray[0][i][0] = dataflow( unwrapped( &ProcessBlockOnColumn ), fsize, hpx::make_ready_future( blockList[i][0] ), *diag_block);
-        //dfArray[0][i][0] = when_all( make_ready_future( blockList[i][0] ), *diag_block).
-        //                        then(unwrapped([](std::vector<hpx::future<block> > const & fs){ return ProcessBlockOnColumn(fs[0].get(), fs[1].get()); }));
         first_col = &dfArray[0][i][0];
         for(int j = 1; j < numBlocks; j++) {
             dfArray[0][i][j] = dataflow( unwrapped( &ProcessInnerBlock ), fsize, hpx::make_ready_future( blockList[i][j]), dfArray[0][0][j], *first_col );
-            //dfArray[0][i][j] = when_all( make_ready_future( blockList[i][j]), dfArray[0][0][j], *first_col ).
-            //                    then(unwrapped([](std::vector<hpx::future<block> > const & fs){ return ProcessInnerBlock(fs[0].get(), fs[1].get(), fs[2].get()); }));
         }
     }
+    //bulk of work, entirely in futures
     for(int i = 1; i < numBlocks; i++) {
         dfArray[i][i][i] = dataflow( unwrapped( &ProcessDiagonalBlock ), fsize, dfArray[i-1][i][i]);
-        //dfArray[i][i][i] = when_all( dfArray[i-1][i][i]).
-        //                        then(unwrapped([](std::vector<hpx::future<block> > const & fs){ return ProcessDiagonalBlock(fs[0].get()); }));
         diag_block = &dfArray[i][i][i];
         for(int j = i + 1; j < numBlocks; j++){
             dfArray[i][i][j] = dataflow( unwrapped(&ProcessBlockOnRow), fsize, dfArray[i-1][i][j], *diag_block);
-            //dfArray[i][i][j] = when_all( dfArray[i-1][i][j], *diag_block).
-            //                    then(unwrapped([](std::vector<hpx::future<block> > const & fs){ return ProcessBlockOnRow(fs[0].get(), fs[1].get()); }));
         }
         for(int j = i + 1; j < numBlocks; j++){
             dfArray[i][j][i] = dataflow( unwrapped( &ProcessBlockOnColumn ), fsize, dfArray[i-1][j][i], *diag_block);
-            //dfArray[i][j][i] = when_all( dfArray[i-1][j][i], *diag_block).
-            //                    then(unwrapped([](std::vector<hpx::future<block> > const & fs){ return ProcessBlockOnColumn(fs[0].get(), fs[1].get()); }));
             first_col = &dfArray[i][j][i];
             for(int k = i + 1; k < numBlocks; k++) {
                 dfArray[i][j][k] = dataflow( unwrapped( &ProcessInnerBlock ), fsize, dfArray[i-1][j][k], dfArray[i][i][k], *first_col );
-                //dfArray[i][j][k] = when_all( dfArray[i-1][j][k], dfArray[i][i][k], *first_col).
-                //                    then(unwrapped([](std::vector<hpx::future<block> > const & fs){ return ProcessInnerBlock(fs[0].get(), fs[1].get(), fs[2].get()); }));
             }
         }
     }
